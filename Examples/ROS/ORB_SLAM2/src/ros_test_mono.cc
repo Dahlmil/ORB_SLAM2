59f36e76a2fd1a4d820bc8d04ce3f863262f9c42
dahlmill <dahlmill@qq.com>
2020-03-18 21:06:12 +0800

    ros update

--- Examples/ROS/ORB_SLAM2/src/ros_test_mono.cc
+++ Examples/ROS/ORB_SLAM2/src/ros_test_mono.cc
@@ -18,34 +18,45 @@
 * along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 #include <fstream>
 #include <chrono>
+#include <string>
 
-#include <ros/ros.h>
-#include <cv_bridge/cv_bridge.h>
-
-#include <opencv2/core/core.hpp>
-
+#include <image_transport/image_transport.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Quaternion.h>
 #include "../../../include/System.h"
-
-#include <sys/time.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/core/core.hpp>
+#include <nav_msgs/Odometry.h>
 #include <opencv2/opencv.hpp>
+#include <std_msgs/String.h>
+#include <nav_msgs/Path.h>
+#include <ros/console.h>
 #include "Converter.h"
 #include <sys/stat.h>
+#include <sys/time.h>
+#include <ros/ros.h>
+
 
 using namespace std;
 using namespace cv;
 using namespace ORB_SLAM2;
 
-#define TUM_PATH "/home/dm/CodeBase/ORB_SLAM2_Master/Examples/Monocular/TUM.yaml"
+// #define TUM_PATH "/home/dm/CodeBase/ORB_SLAM2_Master/Examples/Monocular/TUM.yaml"
+
+#define TUM_PATH "/home/dm/catkin_ws/SLAM_YAML/TUM.yaml"
+
 #define VOC_PATH "/home/dm/CodeBase/ORB_SLAM2_Master/Vocabulary/ORBvoc.bin"
 
 #define VIDEO_MODE 0
 
 long GetCurrentTime(void);
 inline bool FileExists(const std::string &name);
+void SLAM2CameraImage(void);
+void SLAM2UsbCam(void);
 
 class ImageGrabber
 {
@@ -57,76 +68,36 @@ public:
     ORB_SLAM2::System *mpSLAM;
 };
 
+void SLAM2DateSet(void);
+
+void OdomCallback(const nav_msgs::Odometry::ConstPtr &odom);
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "Mono");
     ros::start();
 
-    // if (argc != 3)
-    // {
-    //     cerr << endl
-    //          << "Usage: rosrun ORB_SLAM2 Mono path_to_vocabulary path_to_settings" << endl;
-    //     ros::shutdown();
-    //     return 1;
-    // }
-
-    // Create SLAM system. It initializes all system threads and gets ready to process frames.
-    ORB_SLAM2::System SLAM(VOC_PATH, TUM_PATH, ORB_SLAM2::System::MONOCULAR, true);
-
-#if VIDEO_MODE
-    ImageGrabber igb(&SLAM);
-
-    ros::NodeHandle nodeHandler;
-    ros::Subscriber sub = nodeHandler.subscribe("/usb_cam/image_raw", 1, &ImageGrabber::GrabImage, &igb);
-
-    ros::spin();
-#else
-    int iCount = 0;
-    Mat im;
-    while (1)
+    if (argc != 2)
     {
-        iCount++;
-        ostringstream strPath;
-        strPath << "/home/dm/CodeBase/SLAM_DATA/img/img" << iCount << ".jpg";
-        cout << strPath.str() << endl;
-        if (!FileExists(strPath.str()))
-        {
-            cout << "File NULL" << endl;
-            break;
-        }
-        im = cv::imread(strPath.str());
-
-        double tframe = GetCurrentTime();
-
-        if (im.empty())
-        {
-            return 1;
-        }
-
-        // Pass the image to the SLAM system
-
-        Mat Tcw = SLAM.TrackMonocular(im, tframe);
-        // cout << Tcw.cols << " " << Tcw.rows << endl;
-        if ((Tcw.cols == 4) && (Tcw.rows == 4))
-        {
-            Mat Rwc = Tcw.rowRange(0, 3).colRange(0, 3).t();
-            Mat Twc = -Rwc * Tcw.rowRange(0, 3).col(3);
-            vector<float> q = Converter::toQuaternion(Rwc);
-            cout << "X " << Twc.at<float>(0, 0) * 100 << endl;
-            cout << "Y " << Twc.at<float>(1, 0) * 100 << endl;
-            cout << "Z " << Twc.at<float>(2, 0) * 100 << endl;
-            cout << "------------------" << endl;
-        }
-
-        // usleep(30 * 1000);
+        cout << "mode?" << endl;
+        return 1;
     }
-#endif
 
-    // Stop all threads
-    SLAM.Shutdown();
+    cout << " argv[1] " << argv[1] << endl;
 
-    // Save camera trajectory
-    SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
+    string strArgv = argv[1];
+    if (!strArgv.compare("0"))
+    {
+        SLAM2DateSet();
+    }
+    else if (!strArgv.compare("1"))
+    {
+        SLAM2UsbCam();
+    }
+    else if (!strArgv.compare("2"))
+    {
+        SLAM2CameraImage();
+    }
 
     ros::shutdown();
 
@@ -147,7 +118,18 @@ void ImageGrabber::GrabImage(const sensor_msgs::ImageConstPtr &msg)
         return;
     }
 
-    mpSLAM->TrackMonocular(cv_ptr->image, cv_ptr->header.stamp.toSec());
+    Mat Tcw = mpSLAM->TrackMonocular(cv_ptr->image, cv_ptr->header.stamp.toSec());
+
+    if ((Tcw.cols == 4) && (Tcw.rows == 4))
+    {
+        Mat Rwc = Tcw.rowRange(0, 3).colRange(0, 3).t();
+        Mat Twc = -Rwc * Tcw.rowRange(0, 3).col(3);
+        vector<float> q = Converter::toQuaternion(Rwc);
+        cout << "X " << Twc.at<float>(0, 0) * 100 << endl;
+        cout << "Y " << Twc.at<float>(1, 0) * 100 << endl;
+        cout << "Z " << Twc.at<float>(2, 0) * 100 << endl;
+        cout << "------------------" << endl;
+    }
 }
 
 inline bool FileExists(const std::string &name)
@@ -161,4 +143,89 @@ long GetCurrentTime(void)
     struct timeval tv;
     gettimeofday(&tv, NULL);
     return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
+
+void SLAM2UsbCam(void)
+{
+        ORB_SLAM2::System SLAM(VOC_PATH, TUM_PATH, ORB_SLAM2::System::MONOCULAR, true);
+        ImageGrabber igb(&SLAM);
+
+        ros::NodeHandle nodeHandler;
+        ros::Subscriber sub = nodeHandler.subscribe("/usb_cam/image_raw", 1, &ImageGrabber::GrabImage, &igb);
+
+        ros::spin();
+
+        // Stop all threads
+        SLAM.Shutdown();
+
+        // Save camera trajectory
+        SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
+}
+
+void SLAM2CameraImage(void)
+{
+        ORB_SLAM2::System SLAM(VOC_PATH, TUM_PATH, ORB_SLAM2::System::MONOCULAR, true);
+
+        ImageGrabber igb(&SLAM);
+
+        ros::NodeHandle nodeHandler;
+        ros::Subscriber sub = nodeHandler.subscribe("camera/image", 1, &ImageGrabber::GrabImage, &igb);
+        ros::NodeHandle ph;
+        ros::Subscriber odomSub = ph.subscribe<nav_msgs::Odometry>("/odom", 10, OdomCallback);
+        ros::spin();
+
+        // Stop all threads
+        SLAM.Shutdown();
+
+        // Save camera trajectory
+        SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
+}
+
+void SLAM2DateSet(void)
+{
+    ORB_SLAM2::System SLAM(VOC_PATH, TUM_PATH, ORB_SLAM2::System::MONOCULAR, true);
+    int iCount = 0;
+    Mat im;
+    while (1)
+    {
+        iCount++;
+        ostringstream strPath;
+        strPath << "/home/dm/CodeBase/SLAM_DATA/img/img" << iCount << ".jpg";
+        cout << strPath.str() << endl;
+        if (!FileExists(strPath.str()))
+        {
+            cout << "File NULL" << endl;
+            break;
+        }
+        im = cv::imread(strPath.str());
+
+        double tframe = GetCurrentTime();
+
+        if (im.empty())
+        {
+            return;
+        }
+
+        // Pass the image to the SLAM system
+
+        Mat Tcw = SLAM.TrackMonocular(im, tframe);
+        // cout << Tcw.cols << " " << Tcw.rows << endl;
+        if ((Tcw.cols == 4) && (Tcw.rows == 4))
+        {
+            Mat Rwc = Tcw.rowRange(0, 3).colRange(0, 3).t();
+            Mat Twc = -Rwc * Tcw.rowRange(0, 3).col(3);
+            vector<float> q = Converter::toQuaternion(Rwc);
+            cout << "X " << Twc.at<float>(0, 0) * 100 << endl;
+            cout << "Y " << Twc.at<float>(1, 0) * 100 << endl;
+            cout << "Z " << Twc.at<float>(2, 0) * 100 << endl;
+            cout << "------------------" << endl;
+        }
+
+        usleep(10 * 1000);
+    }
+}
+
+void OdomCallback(const nav_msgs::Odometry::ConstPtr &odom)
+{
+    // ROS_INFO("odom %.3lf %.3lf\n", odom->pose.pose.position.x, odom->pose.pose.position.y);
 }
